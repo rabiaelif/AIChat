1f135658be689f9cce288d0f0655ec27a58ed46f
rabiaelif <rabiaelf09@gmail.com>
2025-11-12 13:02:03 +0300

    Backend Render Dockerfile

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec3f69186a1d373c7669c2e7f68df219ece09124")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+603ea000d453e2434eb755b12433f985c2f3fb28")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
